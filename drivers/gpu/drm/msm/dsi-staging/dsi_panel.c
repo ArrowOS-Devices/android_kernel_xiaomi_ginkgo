0127d9ab455a9a32ffe480f04a26f60da4446d89
Erfan Abdi <erfangplus@gmail.com>
2020-07-20 22:47:18 +0430

    drivers: gpu: Use KTD3136 backlight driver if available

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -619,6 +619,9 @@ static int dsi_panel_wled_register(struct dsi_panel *panel,
 	return 0;
 }
 
+#ifdef CONFIG_BACKLIGHT_KTD3136
+extern int sgm_brightness_set(uint16_t brightness);
+#endif
 static int dsi_panel_update_backlight(struct dsi_panel *panel,
 	u32 bl_lvl)
 {
@@ -632,9 +635,13 @@ static int dsi_panel_update_backlight(struct dsi_panel *panel,
 
 	dsi = &panel->mipi_device;
 
+#ifdef CONFIG_BACKLIGHT_KTD3136
+    sgm_brightness_set(bl_lvl);
+#else
 	rc = mipi_dsi_dcs_set_display_brightness(dsi, bl_lvl);
 	if (rc < 0)
 		pr_err("failed to update dcs backlight:%d\n", bl_lvl);
+#endif
 
 	return rc;
 }
